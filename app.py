ebbde770e544fd23d2f2e71119d28ad8dc675fab
ntksan14-hue <ntksan14@gmail.com>
2026-06-18 01:25:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-import dash
-from dash import html, dcc
+from dash import Dash
+from callbacks.register import register_callbacks
 
-from data import df, df_clean
-from config import SO_COLORS, SO_LABELS, MOD_COLORS, DONUT_COLOR_MAP
+app = Dash(__name__)
+server = app.server
 
-from callbacks import register_callbacks
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True)
-
-app.layout = html.Div([
-    html.Div(id="tab-content"),
-    dcc.Tabs(id="navigation-tabs", value="tab-1", children=[
-        dcc.Tab(label="Tab 1", value="tab-1"),
-        dcc.Tab(label="Tab 2", value="tab-2"),
-        dcc.Tab(label="Tab 3", value="tab-3"),
-        dcc.Tab(label="Tab 4", value="tab-4"),
-    ])
-])
+df = ...
+df_clean = df[df["sp"] == 0]
 
 register_callbacks(app, df, df_clean)
 
-server = app.server   # 🔥 สำคัญสำหรับ Render
-
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run_server(debug=True)
